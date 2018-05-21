9da75b456488e57bc358bb52e078ba4a9562da62
shibuki373 <h-yoshida@toki.waseda.jp>
2018-05-21 20:42:43 +0900

    Responderの管理クラスであるChatBotの追加 変更は主にResponderで行う

--- main.py
+++ main.py
@@ -16,3 +16,34 @@ class Responder:
     def name(self):
         """応答オブジェクトの名前"""
         return self._name
+
+class ChatBot:
+    """チャットボットのコアクラス
+
+    プロパティ:
+    name -- コアの名前
+    responder_name -- 現在の応答クラスの名前
+    """
+
+    def __init__(self, name):
+        """文字列を受け取り、コアインスタンスの名前に設定する
+        'What'Responderインスタンスを作成し、保存する
+        """
+
+        self._name = name
+        self._responder = Responder('What')
+    
+    def dialogue(self, text):
+        """ユーザーからの入力を受け取り、Responderに処理させた結果を返す"""
+
+        return self._responder.response(text)
+
+    @property
+    def name(self):
+        """チャットボットインスタンスの名前"""
+        return self._name
+    
+    @property
+    def responder_name(self):
+        """ 保存しているResponderの名前"""
+        return self._responder.name
